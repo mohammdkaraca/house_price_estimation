2aa20894a47e11c076f847daf5925f7e7bd9f3b2
Mo <mo@Mos-MacBook-Pro.local>
2025-12-10 18:17:02 +0300

    fixing main.py

--- main.py
+++ main.py
@@ -467,7 +467,7 @@ def get_stats():
         }
     return {"error": "Dataset not found"}
 
-app.mount("/", StaticFiles(directory=".", html=True), name="site")    
+app.mount("/", StaticFiles(directory="frontend", html=True), name="site")    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
